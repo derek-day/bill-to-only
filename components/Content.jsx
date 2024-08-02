651cba3c3a8ba7ed94f3eecd6519807f4a9d29b1
Derek Day <derek.andrew.day@gmail.com>
2024-08-02 16:40:05 -0400

    start form

--- components/Content.jsx
+++ components/Content.jsx
@@ -30,30 +30,87 @@ const Content = () => {
       Bill-To-Only
     </h1>
     
-    {user && (
-    <>
-      <div className='mt-5 text-center' data-testid="pdf-items" style={{textAlign:'center'}}>
-        <h4 className="my-4">
-          <a href="/comdue">Comdue</a>
-        </h4>
-        <h4 className="my-4">
-          <a href="/daily">Daily</a>
-        </h4>
-        <h4 className="my-4">
-          <a href="/rcp">Rep Commissions</a>
-        </h4>
-        <h4 className="my-4">
-          <a href="/sample">Sample Bag</a>
-        </h4>
-        <h4 className="my-4">
-          <a href="/territory">Territory Listings</a>
-        </h4>
-        <h4 className="my-4">
-          <a href="/ytd">YTD Critical</a>
-        </h4>
+    {/* {user && (
+    <> */}
+      <div className='mt-5'>
+        <h2>Please use this form to submit a Bill-To-Only order</h2>
+
+        <form>
+
+          <h3><span className='warn'>*</span> Rep ADP #</h3>
+          <input type="text" name="name" placeholder="Enter ADP # of rep" />
+
+          <br/><br/>
+
+          <h3><span className='warn'>*</span> Account # to bill</h3>
+          <input type="text" name="name" placeholder="Enter account # of the customer to bill" />
+
+          <br/><br/>
+
+          <h3><span className='warn'>*</span> Account # to credit</h3>
+          <input type="text" name="name" placeholder="Enter account # of the account to credit" />
+
+          <br/><br/>
+
+          <h3><span className='warn'>*</span> Model</h3>
+          <input type="text" name="name" placeholder="Enter frame model" />
+
+          <br/><br/>
+
+          <h3><span className='warn'>*</span> Color</h3>
+          <input type="text" name="name" placeholder="Enter frame color" />
+
+          <br/><br/>
+
+          <h3><span className='warn'>*</span> Eye</h3>
+          <input type="text" name="name" placeholder="Enter frame eye size" />
+
+          <br/><br/>
+
+          <h3><span className='warn'>*</span> Replace sample?</h3>
+          {/* Yes/No checkbox */}
+            <input type="radio" id="html" name="fav_language" value="Yes" />
+            <label for="yes">Yes</label><br />
+            <input type="radio" id="css" name="fav_language" value="No" />
+            <label for="no">No</label><br />
+
+          <br/><br/>
+
+          <h3><span className='warn'>*</span> Send cases?</h3>
+          {/* Yes/No checkbox */}
+          <input type="radio" id="html" name="fav_language" value="Yes" />
+          <label for="yes">Yes</label><br />
+          <input type="radio" id="css" name="fav_language" value="No" />
+          <label for="no">No</label><br />
+
+          <br/><br/>
+
+          <h3><span className='warn'>*</span> Use courtesy?</h3>
+          {/* Yes/No checkbox */}
+          <input type="radio" id="html" name="fav_language" value="Yes" />
+          <label for="yes">Yes</label><br />
+          <input type="radio" id="css" name="fav_language" value="No" />
+          <label for="no">No</label><br />
+
+          <br/><br/>
+
+          <h3>Comments</h3>
+          {/* Comment box */}
+          <textarea name="comment" form="usrform"></textarea>
+          <h4>Please note that entering comments may delay this Bill-To-Only order due to manual processing.</h4>
+
+          <br/><br/>
+
+          <button type="button">Reset</button>
+          <button type="button">Submit</button>
+
+        </form>
+
+
+        <h4><span className='warn'>*</span> - required fields</h4>
       </div>
-    </>
-    )}
+    {/* </>
+    )} */}
 
   </div>
   )
